e66578c3994975cfdffea5826d70341dfbbd6e65
bashiho <basilrdv74@gmail.com>
2025-02-19 17:03:57 -0800

    Updated some placeholder methods in User.py with potentially useful comments Added accountID attribute in User.py Added attributes to methods that are likely to be useful, eg. email and password are passed through to login

--- User.py
+++ User.py
@@ -5,35 +5,41 @@ password = 'accountPassword'
 email = 'accountEmail'
 phone = 'accountPhone'
 address = 'accountAddress'
+accountID = 'accountID'
 
 # This is a placeholder for the User class
 class User:
-    def __init__(self, name, password, email, phone, address):
+    def __init__(self, name, password, email, phone, address, accountID):
         self.name = name
         self.password = password
         self.email = email
         self.phone = phone
         self.address = address
+        self.accountID = accountID
 
     # View Account Page
-    def accountPage(self):
+    def accountPage(self, accountID):
         # Placeholder for account page method
         pass
 
     # Login to account
-    def login(self):
+    def login(self, email, password):
         # Placeholder for login method
+        # Ensure that there is some way for page to know if login attempt was successful
+        # Check for corrent information
         pass
 
     # Logout of account
     def logout(self):
         # Placeholder for logout method
+        # Ensure that page knows if logout attempt was successful
         pass
     
     # Register an Account
-    def register(self):
+    def register(self, email, password):
         # Placeholder for register method
         # Should use an email and password to register
+        # Could verify account upon creation using email, unnecessary but could be useful addition
         pass
 
     # Update Account Information
@@ -51,11 +57,13 @@ class User:
         pass
 
     # View Orders
-    def viewOrders(self):
+    def viewOrders(self, accountID):
         # Placeholder for view orders method
+        # Could associate accountID with orders in database to allow for filtering
         pass
+
     # View Wishlist
-    def viewWishlist(self):
+    def viewWishlist(self, accountID):
         # Placeholder for view wishlist method
         pass
 
