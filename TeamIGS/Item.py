2a1a2ef96d704c598ac124ed65a9eee5e9d5970f
bashiho <basilrdv74@gmail.com>
2025-02-20 16:04:18 -0800

    Did some initial implementation stuff for item.py and ran django server migrations

--- TeamIGS/Item.py
+++ TeamIGS/Item.py
@@ -1,5 +1,20 @@
 # Handles information of items, such as their price and descriptions, and displaying them to users
-class Item():
-    def __init__(self, name: str, description: str):
-        self.name = name
-        self.description = description
+# Might have to rename to models.py for django, unsure
+from django.db import models
+from django.contrib import admin
+
+class Item(models.Model):
+    #sets max size for attribute fields for products
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    # find way to include product image
+    imageURL = models.CharField(max_length=200)
+    price = models.DecimalField(max_length=100)
+    
+    def __str__(self):
+        return self.name, self.description, self.imageURL, self.price
+    # @admin.display( #can be used to modify admin display
+            
+    # )
+
+    
