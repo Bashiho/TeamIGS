c89dcd92aff3be6c40c8d49651ba04bf3f0aa0a6
bashiho <bashiho@proton.me>
2025-03-06 15:40:34 -0800

    Created simple Cart.py with Cart class

---
+++ TeamIGS/Cart.py
@@ -0,0 +1,14 @@
+from django.db import models
+from .Item import Item
+from .User import User
+
+class Cart(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField(max_length = 10)
+    price = models.IntegerField(max_length = 50)
+
+
+    # placeholder method to be called when viewing cart
+    def viewCart(self):
+        return items_in_cart
