6d5c883552bee68aa9dd4d01289f0171fc6bdfd7
Bashiho <basilrdv74@gmail.com>
2025-03-30 20:36:04 -0700

    Minor updates to Item.py and Category.py, Item image is not read properly in the html and categories still don't display items properly on its page, requires fixing

--- TeamIGS/Category.py
+++ TeamIGS/Category.py
@@ -3,6 +3,17 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    # allows for alternate url that includes name of category when category is multiple words, requires implementation
+    slug = models.SlugField(max_length=200, unique=True, default=1)
+
+    class Meta:
+        ordering = ('name',)
+
+
+    # returns all categories
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
 
     def __str__(self):
         return self.name
--- TeamIGS/Item.py
+++ TeamIGS/Item.py
@@ -6,9 +6,14 @@ from .Category import Category
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='TeamIGS/ItemPictures', default=1) # Allows admin to upload image for product when creating product
+    image = models.ImageField(upload_to='TeamIGS/static/ItemPictures', default=1) # Need to fix upload location, html doesn't retrieve images properly
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    slug = models.SlugField(max_length=200, db_index=True, default=1)
+
+    class Meta:
+        ordering = ('name',)
+        # index_together = (('id', 'slug'),) seems to cause errors, not sure why
     
     # Shows Items by name instead of ID when working in DB
     def __str__(self):
